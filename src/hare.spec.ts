7f1d1abfc037c5bbe4000e80b863b16f3d8890ab
Gabriel Vieira <gluisvieira@gmail.com>
2017-11-11 06:33:42 -0200

    add first mocha test case (green)

--- src/hare.spec.ts
+++ src/hare.spec.ts
@@ -7,6 +7,12 @@ describe('.tokenize()', () => {
   it('should return an array of tokens', () => {
     const str = "(hello 1 2 3)";
     const tokens = hare.tokenize(str);
-    expect(tokens).to.equal(['(', 'hello', '1', '2', '3', ')']);
+    expect(tokens.length == str.length);
+    expect(tokens[0]).to.equal('(');
+    expect(tokens[1]).to.equal('hello');
+    expect(tokens[2]).to.equal('1');
+    expect(tokens[3]).to.equal('2');
+    expect(tokens[4]).to.equal('3');
+    expect(tokens[5]).to.equal(')');
   });
 });
