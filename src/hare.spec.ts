2b77e55553f69b89f47608aa3347f981db973437
Gabriel Vieira <gluisvieira@gmail.com>
2017-11-11 06:40:59 -0200

    Refactor format code according to my own style guide

--- src/hare.spec.ts
+++ src/hare.spec.ts
@@ -3,16 +3,23 @@ import * as _ from "lodash";
 import { expect } from "chai";
 import "mocha";
 
-describe('.tokenize()', () => {
-  it('should return an array of tokens', () => {
+describe( '.tokenize()', () =>
+{
+  it( 'should return an array of tokens', () =>
+  {
     const str = "(hello 1 2 3)";
-    const tokens = hare.tokenize(str);
-    expect(tokens.length == str.length);
-    expect(tokens[0]).to.equal('(');
-    expect(tokens[1]).to.equal('hello');
-    expect(tokens[2]).to.equal('1');
-    expect(tokens[3]).to.equal('2');
-    expect(tokens[4]).to.equal('3');
-    expect(tokens[5]).to.equal(')');
-  });
-});
+    const tokens = hare.tokenize( str );
+    //
+    // We expect tokens to be this:
+    //
+    //    ["(", "hello", "1", "2", "3", ")"]
+    //
+    expect( tokens.length ).to.equal( 6 ); // = 2 parens + 4 atoms
+    expect( tokens[0] ).to.equal( '(' );
+    expect( tokens[1] ).to.equal( 'hello' );
+    expect( tokens[2] ).to.equal( '1' );
+    expect( tokens[3] ).to.equal( '2' );
+    expect( tokens[4] ).to.equal( '3' );
+    expect( tokens[5] ).to.equal( ')' );
+  } );
+} );
