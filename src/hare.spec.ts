5a3549d6a749466b916f220941e0c84ef6d873a3
Gabriel Vieira <gluisvieira@gmail.com>
2017-11-15 14:41:46 -0200

    Add another test case and refactor previous one

--- src/hare.spec.ts
+++ src/hare.spec.ts
@@ -5,15 +5,14 @@ import "mocha";
 
 describe( '.tokenize()', () =>
 {
+  //////////////////////////////////////////////////////////////////////////////
   it( 'should work with a flat list string', () =>
   {
+    // setup
     const str = "(hello 1 2 3)";
+    // stress
     const tokens = hare.tokenize( str );
-    //
-    // We expect `tokens` to be:
-    //
-    //    ["(", "hello", "1", "2", "3", ")"]
-    //
+    // test
     expect( tokens.length ).to.equal( 6 );
     expect( tokens[0] ).to.equal( '(' );
     expect( tokens[1] ).to.equal( 'hello' );
@@ -22,28 +21,30 @@ describe( '.tokenize()', () =>
     expect( tokens[4] ).to.equal( '3' );
     expect( tokens[5] ).to.equal( ')' );
   });
+  //////////////////////////////////////////////////////////////////////////////
   it('should work with a nested list string', () =>
   {
-    const str = "(hello 1 (2 3) (4 5) 6)";
+    // setup
+    const str = "((1) (2 (3) 4) (5 (6)) 7)";
+    const expectedTokens = [
+      '(', '(', '1', ')', '(', '2', '(', '3', ')', '4', ')', '(', '5', '(', '6', ')', ')', '7', ')'
+    ];
+    // stress
     const tokens = hare.tokenize(str);
-    //
-    // We expect `tokens` to be:
-    //
-    //    ["(", "hello", "1", "(", "2", "3", ")", "(", "4", "5", ")", "6"]
-    //
-    expect( tokens.length ).to.equal( 13 );
-    expect( tokens[0] ).to.equal( '(' );
-    expect( tokens[1] ).to.equal( 'hello' );
-    expect( tokens[2] ).to.equal( '1' );
-    expect( tokens[3] ).to.equal( '(' );
-    expect( tokens[4] ).to.equal( '2' );
-    expect( tokens[5] ).to.equal( '3' );
-    expect( tokens[6] ).to.equal( ')' );
-    expect( tokens[7] ).to.equal( '(' );
-    expect( tokens[8] ).to.equal( '4' );
-    expect( tokens[9] ).to.equal( '5' );
-    expect( tokens[10] ).to.equal( ')' );
-    expect( tokens[11] ).to.equal( '6' );
-    expect( tokens[12] ).to.equal( ')' );
+    // test
+    expect( tokens ).to.deep.equal( expectedTokens );
+  });
+  //////////////////////////////////////////////////////////////////////////////
+  it('should work with a list that has empty lists', () =>
+  {
+    // setup
+    const str = "(() hello (1) (() 3 ()))";
+    const expectedTokens = [
+      '(', '(', ')', 'hello', '(', '1', ')', '(', '(', ')', '3', '(', ')', ')', ')'
+    ];
+    // stress
+    const tokens = hare.tokenize(str);
+    // test
+    expect( tokens ).to.deep.equal( expectedTokens );
   });
 });
