43865fa7b061238536c32fa66b5a723c1baa660a
Gabriel Vieira <gluisvieira@gmail.com>
2017-12-14 17:02:07 -0200

    Add two more failing specs to editor

--- src/editor.spec.ts
+++ src/editor.spec.ts
@@ -12,7 +12,29 @@ describe( '.replace()', () =>
     const array = ['foo', 'bar', 'baz']
     const indexes:Array<number> = []
     // stress
-    const array2 = editor.replace(array, indexes)
+    const array2 = editor.replace(array, indexes, 'quux')
+    // test
+    expect( array2 ).to.deep.equal( array );
+  });
+  //////////////////////////////////////////////////////////////////////////////
+  it( 'should change one element for a single index', () =>
+  {
+    // setup
+    const array = ['foo', 'bar', 'baz']
+    const indexes:Array<number> = [1]
+    // stress
+    const array2 = editor.replace(array, indexes, 'quux')
+    // test
+    expect( array2 ).to.deep.equal( ['foo', 'quux', 'baz'] );
+  });
+  //////////////////////////////////////////////////////////////////////////////
+  it( 'should not change anything for an out of bounds index', () =>
+  {
+    // setup
+    const array = ['foo', 'bar', 'baz']
+    const indexes:Array<number> = [3]
+    // stress
+    const array2 = editor.replace(array, indexes, 'quux')
     // test
     expect( array2 ).to.deep.equal( array );
   });
--- src/editor.ts
+++ src/editor.ts
@@ -1,5 +1,5 @@
 import * as _ from "lodash"
 
-export function replace(array: Array<any>, indexes: Array<number>) {
+export function replace(array: Array<any>, indexes: Array<number>, element: any) {
   return array
 }
